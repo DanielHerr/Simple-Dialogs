bd0080365b15668b53b6b6e16781a919a3c31554
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-02-12 14:24:36 -0500

    Update simpledialogs.js

--- simpledialogs.js
+++ simpledialogs.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function tell(message, options) {
- let promise = new Promise(function(resolve) {
+ return(new Promise(function(resolve) {
   if(typeof(options) == "function") {
    callback = options
    options = {}
@@ -17,38 +17,42 @@ function tell(message, options) {
    dialog.close()
   })
   dialog.appendChild(close)
-  dialog.addEventListener("close", resolve)
+  dialog.addEventListener("close", function() {
+   dialog.remove()
+   resolve()
+  })
   document.body.appendChild(dialog)
   dialog.showModal()
- })
- return(promise)
-}
+})) }
 
 function ask(message, options) {
- let promise = new Promise(function(resolve, reject) {
+ return(new Promise(function(resolve, reject) {
   if(typeof options === "function") {
    callback = options
    options = {}
   }
   options = options || {}
+  options.yes = options.yes || {}
+  options.no = options.no || {}
   let dialog = document.createElement("dialog")
   dialog.textContent = message
   dialog.innerHTML = dialog.innerHTML.split("\n").join("<br>") + "<br><br>" 
   let yes = document.createElement("button")
-  yes.textContent = options.yes.text || "Yes"
+  yes.textContent = options.yes.text || "Continue"
   yes.style.float = options.yes.side || "left"
   yes.addEventListener("click", function() {
    dialog.close(true)
   })
   dialog.appendChild(yes)
   let no = document.createElement("button")
-  no.textContent = options.no.text || "No"
+  no.textContent = options.no.text || "Cancel"
   no.style.float = options.no.side || "right"
   no.addEventListener("click", function() {
    dialog.close()
   })
   dialog.appendChild(no)
   dialog.addEventListener("close", function() {
+   dialog.remove()
    if(dialog.returnValue) {
     resolve()
    } else {
@@ -56,6 +60,4 @@ function ask(message, options) {
   } })
   document.body.appendChild(dialog)
   dialog.showModal()
- })
- return(promise)
-}
+})) }
