4519431a9680d07e756e28b1a9af8e3f14ae7b73
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-03-08 10:55:37 -0500

    Update simpledialogs.js

--- simpledialogs.js
+++ simpledialogs.js
@@ -1,63 +1,54 @@
 "use strict"
 
-function tell(message, options) {
+function tell(message, { closetext = "Close", closeside = "right" } = {}) {
  return(new Promise(function(resolve) {
-  if(typeof(options) == "function") {
-   callback = options
-   options = {}
-  }
-  options = options || {}
   let dialog = document.createElement("dialog")
   dialog.textContent = message
   dialog.innerHTML = dialog.innerHTML.split("\n").join("<br>") + "<br><br>"
   let close = document.createElement("button")
-  close.textContent = options.text || "Continue"
-  close.style.float = options.side || "right"
+  close.textContent = closetext
+  close.style.float = closeside
   close.addEventListener("click", function() {
    dialog.close()
   })
   dialog.appendChild(close)
   dialog.addEventListener("close", function() {
-   dialog.remove()
    resolve()
+   dialog.remove()
   })
   document.body.appendChild(dialog)
   dialog.showModal()
 })) }
 
-function ask(message, options) {
+function ask(message, { yestext = "Yes", yesside = "left", notext = "No", noside = "right", noerror = true } = {}) {
  return(new Promise(function(resolve, reject) {
-  if(typeof options === "function") {
-   callback = options
-   options = {}
-  }
-  options = options || {}
-  options.yes = options.yes || {}
-  options.no = options.no || {}
   let dialog = document.createElement("dialog")
   dialog.textContent = message
   dialog.innerHTML = dialog.innerHTML.split("\n").join("<br>") + "<br><br>" 
   let yes = document.createElement("button")
-  yes.textContent = options.yes.text || "Continue"
-  yes.style.float = options.yes.side || "left"
+  yes.textContent = yestext
+  yes.style.float = yesside
   yes.addEventListener("click", function() {
    dialog.close(true)
   })
   dialog.appendChild(yes)
   let no = document.createElement("button")
-  no.textContent = options.no.text || "Cancel"
-  no.style.float = options.no.side || "right"
+  no.textContent = notext
+  no.style.float = noside
   no.addEventListener("click", function() {
    dialog.close()
   })
   dialog.appendChild(no)
   dialog.addEventListener("close", function() {
-   dialog.remove()
    if(dialog.returnValue) {
-    resolve()
+    resolve(true)
+   } else if(noerror) {
+    reject(false)
    } else {
-    reject()
-  } })
+    resolve(false)
+   }
+   dialog.remove()
+  })
   document.body.appendChild(dialog)
   dialog.showModal()
 })) }
